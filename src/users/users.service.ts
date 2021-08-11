41bf06d3258468cdb46f5e616e878483ce3aac5c
Gabriel Barreto <gabriel.devloper@gmail.com>
2021-08-11 13:52:47 -0300

    feat: criação dos metodos de ações de users.service.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,16 +7,24 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {
-  }
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
     const user = this.userRepository.create(createUserDto);
 
-    const userAlreadyExists = await this.userRepository.findOne(user.id);
+    const userAlreadyExists = await this.userRepository.findOne({
+      where: {
+        cpf: user.cpf,
+      },
+    });
 
-    if(userAlreadyExists){
-      throw new HttpException('Já existe um usuario cadastrado com este CPF', HttpStatus.BAD_REQUEST);
+    if (!!userAlreadyExists) {
+      throw new HttpException(
+        'Já existe um usuario cadastrado com este CPF',
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
     await this.userRepository.save(user);
@@ -31,15 +39,44 @@ export class UsersService {
     return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne(id);
+
+    if (!user) {
+      throw new HttpException(
+        'Nenhum usuário foi encontrado',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    delete user.password;
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const userAlreadyExist = await this.userRepository.findOne(id);
+
+    if (!userAlreadyExist) {
+      throw new HttpException(`Usuário não encontrado`, HttpStatus.NOT_FOUND);
+    }
+
+    const user = this.userRepository.create(updateUserDto);
+
+    await this.userRepository.update(id, user);
+
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const userAlreadyExist = await this.userRepository.findOne(id);
+
+    if (!userAlreadyExist) {
+      throw new HttpException(`Usuário não encontrado`, HttpStatus.NOT_FOUND);
+    }
+
+    await this.userRepository.remove(userAlreadyExist);
+
+    return;
   }
 }
