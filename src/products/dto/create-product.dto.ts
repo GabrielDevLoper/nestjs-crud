877941d8286349f05fc9d2769848d0763349732c
Gabriel Barreto <gabriel.devloper@gmail.com>
2021-08-11 01:36:55 -0300

    feat: entidade products criada e configurado o typeorm.

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  readonly description: string;
+
+  @IsNumber()
+  readonly id_category: number;
+
+  @IsNumber()
+  readonly id_user: number;
+
+  @IsNumber()
+  readonly amount: number;
+
+  @IsNumber()
+  readonly price: number;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,31 @@
-export class Product {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  id_category: number;
+
+  @Column()
+  id_user: number;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  price: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+
+
+}
